37086dc88b87cbf85f66aadc074ee178c5ff1f2c
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-03-09 11:03:46 -0600

    Explore the wonderful world of strongly typed JS

---
+++ ts1/main.ts
@@ -0,0 +1,17 @@
+class Point {
+  x: number;
+  y: number;
+
+  draw() {
+    console.log(`X: ${this.x}, Y: ${this.y}`);
+  }
+
+  getDistance(another: Point) {
+    // ..
+  }
+}
+let point = new Point();
+let cord;
+Point;
+point.x = 5;
+console.log(point);

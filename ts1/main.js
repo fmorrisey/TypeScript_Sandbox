68a7fa97545c6957a6f503e1ca15f027e56d1f98
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-03-09 15:09:02 -0600

    Tranpiled to ES5

--- ts1/main.js
+++ ts1/main.js
@@ -1,5 +1,5 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 var like_component_1 = require("./like.component");
 var like = new like_component_1.LikeComponent(true, 10);
 like.onClick();
---
+++ ts1/point.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Point = void 0;
+var Point = /** @class */ (function () {
+    function Point(_x, _y) {
+        this._x = _x;
+        this._y = _y;
+        //   ? makes params optional
+    }
+    Point.prototype.draw = function () {
+        console.log("X: " + this._x + ", Y: " + this._y);
+    };
+    Object.defineProperty(Point.prototype, "x", {
+        get: function () {
+            return this._x;
+        },
+        set: function (value) {
+            if (value < 0) {
+                throw new Error("Value cannot be less than zero");
+            }
+            this._x = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Point;
+}());
+exports.Point = Point;

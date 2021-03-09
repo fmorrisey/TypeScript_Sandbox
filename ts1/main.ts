85d8378de4034ad4fcff5b460fa46634f2e3f5d0
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-03-09 11:23:07 -0600

    Getter and Setter methods

--- ts1/main.ts
+++ ts1/main.ts
@@ -1,12 +1,27 @@
 class Point {
-  constructor(public x?: number, public y?: number) {
+  constructor(private x?: number, private y?: number) {
     //   ? makes params optional
   }
 
   draw() {
     console.log(`X: ${this.x}, Y: ${this.y}`);
   }
+
+  get X() {
+    return this.x;
+  }
+  set X(value) {
+    if (value < 0) {
+      throw new Error("Value cannot be less than zero");
+    }
+    this.x = value;
+  }
+  getY() {
+    return this.y;
+  }
 }
 
 let point = new Point(6, 5);
+let x = point.X;
+point.X = 10;
 point.draw();

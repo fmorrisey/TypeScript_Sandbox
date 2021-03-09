64457432e8df839e1fbea53ae96279c1a69aceda
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-03-09 11:11:21 -0600

    created a constuctor method

--- ts1/main.ts
+++ ts1/main.ts
@@ -3,14 +3,15 @@ class Point {
   x: number;
   y: number;
 
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
   draw() {
     console.log(`X: ${this.x}, Y: ${this.y}`);
   }
-
-  getDistance(another: Point) {
-    // ..
-  }
 }
 
-let point = new Point();
+let point = new Point(6, 5);
 point.draw();

354a2a7a8dd84a40981b4b59e6d7a7eb7895ca90
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-03-09 11:29:11 -0600

    point imported and broken off into a seperate file

--- ts1/main.ts
+++ ts1/main.ts
@@ -1,22 +1,4 @@
-class Point {
-  constructor(private _x?: number, private _y?: number) {
-    //   ? makes params optional
-  }
-
-  draw() {
-    console.log(`X: ${this._x}, Y: ${this._y}`);
-  }
-
-  get x() {
-    return this._x;
-  }
-  set x(value) {
-    if (value < 0) {
-      throw new Error("Value cannot be less than zero");
-    }
-    this._x = value;
-  }
-}
+import { Point } from "./point";
 
 let point = new Point(6, 5);
 let x = point.x;
